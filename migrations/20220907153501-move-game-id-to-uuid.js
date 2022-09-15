3a55ad960f2a894083a5aa46f6eab3d7ed0453fe
Evan Snyder <evan@larky.com>
2022-09-15 10:19:38 -0700

    Bulk of the migration from game id to game uuid

--- migrations/20220907153501-move-game-id-to-uuid.js
+++ migrations/20220907153501-move-game-id-to-uuid.js
@@ -13,30 +13,108 @@ module.exports = {
       primaryKey: true,
     });
     const games = await models.game.findAll({})
+    const gameIdToUuidMap = {}
     for (const game of games) {
-      console.log('whats this?', game)
+      console.log('whats this?', game.id)
+      const myUuid = uuidv4();
+      gameIdToUuidMap[game.id] = myUuid
       await game.update({
-        uuid: uuidv4(),
+        uuid: myUuid,
       })
     }
-    return
-    // return queryInterface.addConstraint(
-    //   'turns',
-    //   ['gameId'],
-    //   {
-    //     type: 'foreign key',
-    //     name: 'gameId_fk',
-    //     references: {
-    //       table: 'games',
-    //       field: 'id'
-    //     },
-    //     onDelete: 'cascade',
-    //     onUpdate: 'cascade'
-    //   }
-    // )
+    console.log(gameIdToUuidMap)
+    await queryInterface.removeConstraint(
+      'turns',
+      'gameId_fk',
+      {},
+    )
+    await queryInterface.removeConstraint(
+      'games',
+      'games_pkey',
+      {},
+    );
+    await queryInterface.addConstraint(
+      'games',
+      ['uuid'],
+      {
+        type: 'primary key',
+        name: 'games_pkey',
+      }
+    )
+    let turns = await models.turn.findAll({})
+    const turnIdToUuidMap = {}
+    for (const turn of turns) {
+      turnIdToUuidMap[turn.id] = gameIdToUuidMap[turn.gameId]
+    }
+    console.log(turnIdToUuidMap)
+
+    await queryInterface.removeColumn('turns', 'gameId')
+    await queryInterface.addColumn('turns', 'gameId', { 
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      // allowNull: false,
+    });
+    turns = await models.turn.findAll({}) // todo: unnecessary?
+    for (const turn of turns) {
+      const uuidForTurn = turnIdToUuidMap[turn.id]
+      await turn.update({
+        gameId: uuidForTurn,
+      })
+    }
+    return queryInterface.addConstraint(
+      'turns',
+      ['gameId'],
+      {
+        type: 'foreign key',
+        name: 'gameId_fk',
+        references: {
+          table: 'games',
+          field: 'uuid'
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade'
+      }
+    )
+    // TODO: null constraints
   },
 
-  down: (queryInterface, Sequelize) => {
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeConstraint(
+      'turns',
+      'gameId_fk'
+    )
+    await queryInterface.removeColumn('turns', 'gameId')
+    await queryInterface.addColumn('turns', 'gameId', {
+      type: DataTypes.INTEGER,
+    })
+    await queryInterface.removeConstraint(
+      'games',
+      'games_pkey',
+    )
+    await queryInterface.addConstraint(
+      'games',
+      ['id'],
+      {
+        type: 'primary key',
+        name: 'games_pkey',
+      }
+    )
+    await queryInterface.addConstraint(
+      'turns',
+      ['gameId'],
+      {
+        type: 'foreign key',
+        name: 'gameId_fk',
+        references: {
+          table: 'games',
+          field: 'id'
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade'
+      }
+    )
+
+    // TODO: Didn't work?
     return queryInterface.removeColumn('games', 'uuid');
     // return queryInterface.removeConstraint(
     //   'turns',
