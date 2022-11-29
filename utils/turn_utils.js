3e85a47d5cc9aedaf7c646c9f47c13e05050263b
Evan Snyder <evan@larky.com>
2022-11-29 14:20:32 -0800

    Uses more logical prompts for messages

--- utils/turn_utils.js
+++ utils/turn_utils.js
@@ -10,7 +10,7 @@ const models = require('../models');
 const utils = require('../utils/utils');
 
 const textReg = /[a-zA-Z0-9\.\!\+\$\#\@\_\&\-\+\(\)\/\*\"\'\:\;\!\?\~\`\|\•\√\π\÷\×\¶\∆\£\¢\€\¥\^\°\=\{\}\\\]\[\✓\%\<\>\%\/\*\-\+\ç\ß\à\á\â\ä\æ\ã\å\ā\è\é\ē\ê\ë\û\ú\ù\ü\ū\î\ì\ï\í\ī\ó\ø\œ\ō\ô\ö\õ\ò\ñ]+/
-const FIRST_TURN_PROMPT = "Welcome to Emojiphone! You are the first player, so all you need to do is respond with a phrase or sentence that is easy to describe with emojis!";
+const FIRST_TURN_PROMPT = "Welcome to Emojiphone! You are the first player, so all you need to do is respond to this text with a phrase or sentence that is easy to describe with emojis!";
 
 module.exports = {
     RESTART_KEYWORD: 'again',
@@ -133,7 +133,7 @@ If you'd like to restart your latest game, simply send a message to this number 
         if (!previousTurn) {
             return FIRST_TURN_PROMPT;
         } else {
-            return `Text your response to the following prompt using ONLY ${currentMessageType}:
+            return `Respond to this text by translating the following message into ONLY ${currentMessageType}:
 ${previousTurn.message}`
         }
     }
