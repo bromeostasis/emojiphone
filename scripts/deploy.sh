e53794fa496085620bc732315ab6c321905cd52e
Evan Snyder <evan@larky.com>
2022-12-08 12:49:59 -0800

    New hash to deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,9 @@
-echo "docker build -t emojiphone ."
+docker build -t emojiphone .
 HASH=$(git rev-parse --short HEAD | tr -d '\n' )
-echo "docker tag emojiphone:latest 956918984222.dkr.ecr.us-east-2.amazonaws.com/emojiphone:$HASH"
-echo "docker push 956918984222.dkr.ecr.us-east-2.amazonaws.com/emojiphone:$HASH"
+docker tag emojiphone:latest 956918984222.dkr.ecr.us-east-2.amazonaws.com/emojiphone:$HASH
+docker push 956918984222.dkr.ecr.us-east-2.amazonaws.com/emojiphone:$HASH
 
-# CREATE_TASK_COMMAND=$(node scripts/editConfig.js $HASH)
-# echo "$CREATE_TASK_COMMAND"
-# export TASK_VERSION=$($CREATE_TASK_COMMAND | jq --raw-output '.taskDefinition.revision')
-# aws ecs register-task-definition --family emojiphoneec --container-definitions "[{\"name\":\"emojiphone\",\"image\":\"956918984222.dkr.ecr.us-east-2.amazonaws.com/emojiphone:e60b5d4\",\"cpu\":0,\"memory\":498,\"links\":[],\"portMappings\":[{\"containerPort\":3000,\"hostPort\":80,\"protocol\":\"tcp\"}],\"essential\":true,\"entryPoint\":[],\"command\":[],\"environment\":[{\"name\":\"NODE_ENV\",\"value\":\"staging\"}],\"environmentFiles\":[],\"mountPoints\":[],\"volumesFrom\":[],\"secrets\":[],\"dnsServers\":[],\"dnsSearchDomains\":[],\"extraHosts\":[],\"dockerSecurityOptions\":[],\"dockerLabels\":{},\"ulimits\":[],\"systemControls\":[]}]" --execution-role-arn arn:aws:iam::956918984222:role/ecsTaskExecutionRole --requires-compatibilities EC2 --tags "[{\"key\":\"ecs:taskDefinition:createdFrom\",\"value\":\"ecs-console-v2\"}]"
-echo 'aws ecs register-task-definition --family emojiphoneec --container-definitions "config/taskDefinition$HASH.json"  --execution-role-arn arn:aws:iam::956918984222:role/ecsTaskExecutionRole --requires-compatibilities EC2 --tags "[{\"key\":\"ecs:taskDefinition:createdFrom\",\"value\":\"ecs-console-v2\"}]"'
-aws ecs register-task-definition --family emojiphoneec --container-definitions "config/taskDefinition$HASH.json"  --execution-role-arn arn:aws:iam::956918984222:role/ecsTaskExecutionRole --requires-compatibilities EC2 --tags "[{\"key\":\"ecs:taskDefinition:createdFrom\",\"value\":\"ecs-console-v2\"}]"
-echo "aws ecs update-service --cluster emojiphonessh --service mojiphone-lb --task-definition emojiphoneec:$TASK_VERSION"
+node scripts/editConfig.js $HASH
+export TASK_VERSION=$(aws ecs register-task-definition --cli-input-json file://scripts/config/taskDefinition$HASH.json | jq --raw-output '.taskDefinition.revision')
+echo "TASK_VERSION $TASK_VERSION"
+aws ecs update-service --cluster emojiphonessh --service mojiphone-lb --task-definition emojiphoneec:$TASK_VERSION
--- scripts/editConfig.js
+++ scripts/editConfig.js
@@ -12,14 +12,14 @@ taskDefinition.containerDefinitions[0].image = `956918984222.dkr.ecr.us-east-2.a
 
 // console.log(taskDefinition);
 
-const {
-	family,
-	containerDefinitions,
-	executionRoleArn,
-	requiresCompatibilities,
-	tags
-} = taskDefinition
+// const {
+// 	family,
+// 	containerDefinitions,
+// 	executionRoleArn,
+// 	requiresCompatibilities,
+// 	tags
+// } = taskDefinition
 
-console.log(`aws ecs register-task-definition --family ${family} --container-definitions "${JSON.stringify(containerDefinitions).replaceAll('"', '\\"')}" --execution-role-arn ${executionRoleArn} --requires-compatibilities ${requiresCompatibilities} --tags "${JSON.stringify(tags).replaceAll('"', '\\"')}"`)
+// console.log(`aws ecs register-task-definition --family ${family} --container-definitions "${JSON.stringify(containerDefinitions).replaceAll('"', '\\"')}" --execution-role-arn ${executionRoleArn} --requires-compatibilities ${requiresCompatibilities} --tags "${JSON.stringify(tags).replaceAll('"', '\\"')}"`)
 
 fs.writeFileSync(`scripts/config/taskDefinition${myArgs[0]}.json`, JSON.stringify(taskDefinition))
