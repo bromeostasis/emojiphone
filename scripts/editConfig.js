a2ce058c115134a3cace96462fc73ee02af012d3
Evan Snyder <evan@larky.com>
2022-12-08 12:26:24 -0800

    Most of auto-deployment done; having trouble with escape characters in create task definition command

---
+++ scripts/editConfig.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const fs = require('fs');
+
+const myArgs = process.argv.slice(2);
+// console.log('myArgs: ', myArgs);
+
+let rawdata = fs.readFileSync('scripts/config/taskDefinition.json'); // TODO: Localize?!
+let taskDefinition = JSON.parse(rawdata);
+
+taskDefinition.containerDefinitions[0].image = `956918984222.dkr.ecr.us-east-2.amazonaws.com/emojiphone:${myArgs[0]}`
+
+// console.log(taskDefinition);
+
+const {
+	family,
+	containerDefinitions,
+	executionRoleArn,
+	requiresCompatibilities,
+	tags
+} = taskDefinition
+
+console.log(`aws ecs register-task-definition --family ${family} --container-definitions "${JSON.stringify(containerDefinitions).replaceAll('"', '\\"')}" --execution-role-arn ${executionRoleArn} --requires-compatibilities ${requiresCompatibilities} --tags "${JSON.stringify(tags).replaceAll('"', '\\"')}"`)
+
+fs.writeFileSync(`scripts/config/taskDefinition${myArgs[0]}.json`, JSON.stringify(taskDefinition))
