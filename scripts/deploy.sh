42cfa7a396f17b236eacb6fd5525ac35bdccbf7c
Evan Snyder <evan@larky.com>
2022-12-13 10:16:25 -0800

    Cleanup

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,5 +5,5 @@ docker push 956918984222.dkr.ecr.us-east-2.amazonaws.com/emojiphone:$HASH
 
 node scripts/editConfig.js $HASH
 export TASK_VERSION=$(aws ecs register-task-definition --cli-input-json file://scripts/config/taskDefinition$HASH.json | jq --raw-output '.taskDefinition.revision')
-echo "TASK_VERSION $TASK_VERSION"
+echo "Created TASK_VERSION $TASK_VERSION"
 aws ecs update-service --cluster emojiphonessh --service mojiphone-lb --task-definition emojiphoneec:$TASK_VERSION
