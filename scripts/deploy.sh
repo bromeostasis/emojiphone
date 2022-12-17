f4a460d4487ca82d14786a9d0505c5cd34a53705
Evan Snyder <evan@larky.com>
2022-12-16 17:33:14 -0800

    Adds docker login to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,7 @@
+
+# Def only works locally :)
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 956918984222.dkr.ecr.us-east-2.amazonaws.com/emojiphone
+
 docker build -t emojiphone .
 HASH=$(git rev-parse --short HEAD | tr -d '\n' )
 docker tag emojiphone:latest 956918984222.dkr.ecr.us-east-2.amazonaws.com/emojiphone:$HASH
