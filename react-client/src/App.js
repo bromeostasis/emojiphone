1c7f3e2763a8183ea30edcecb3aa18c185e81073
Evan Snyder <evan@larky.com>
2023-01-04 15:54:24 -0500

    Fixes to fran container at full width

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -41,9 +41,9 @@ function App() {
         <StartGame />
         <EmojiSpacer />
         <Feedback />
-        <FRANMAYO />
         {/*Backend's working? {message}*/}
       </Container>
+      <FRANMAYO />
     </div>
   );
 }
--- react-client/src/components/FranMayo.jsx
+++ react-client/src/components/FranMayo.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
 
 const FRANMAYO = (props) => {
   return (
-    <Row className='fran mt-3 pt-2 pb-2'>
-    	<Col>
-    		Website design by <a className='inlineLink' href='https://www.fran.land/' target='_blank'>Fran</a>
-    	</Col>
-    </Row>
+  	<Container className='fran mt-3 pt-3 pb-3' fluid>
+	    <Row >
+	    	<Col>
+	    		Website design by <a className='inlineLink' href='https://www.fran.land/' target='_blank'>Fran</a>
+	    	</Col>
+	    </Row>
+	</Container>
   )
 }
 
