e53ad85478cfb4751ebd1567997fbf8fbc2448a6
Evan Snyder <evan@larky.com>
2023-02-24 16:10:04 -0800

    Adds instagram link

--- react-client/src/components/FranMayo.jsx
+++ react-client/src/components/FranMayo.jsx
@@ -3,13 +3,19 @@ import React from 'react';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
+import instagram from './FranMayo/instagram.png'
 
 const FRANMAYO = (props) => {
   return (
   	<Container className='fran mt-3 pt-3 pb-3' fluid>
 	    <Row>
-	    	<Col>
+	    	<Col xs={8}>
 	    		Website design by <a className='inlineLink' rel="noreferrer" href='https://www.fran.land/' target='_blank'>Fran</a>
+	    	</Col>
+	    	<Col xs={4} className='d-flex justify-content-end align-items-center'>
+	    		<a className='imageLink' href='https://www.instagram.com/mojiphone.fun/' target='_blank'>
+	    			<img src={instagram} />
+	    		</a>
 	    	</Col>
 	    </Row>
 	</Container>
